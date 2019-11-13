1199b93811bdbadd191a1372e426c36e16cfc367
ElementG9 <elementg9dev@gmail.com>
2019-11-13 10:26:27 -0700

    Add dark mode

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
+// Create the app.
 const app = require('express')();
+// Import dependencies.
+const helmet = require('helmet');
+const compression = require('compression');
+// Configure dependencies.
+app.use(helmet());
+app.use(compression());
+app.set('json spaces', 2);
+app.set('view engine', 'pug');
+app.set('views', `${__dirname}/views/`);
+
+// The routes.
 app.get('/', (req, res) => {
-  res.send('Hello world!');
+  res.render('index');
 });
+
+// Serve static files.
+app.get('/favicon.ico', (req, res) => {
+  res.status(404);
+});
+app.get('/css/:file', (req, res) => {
+  res.sendFile(`${__dirname}/public/css/${req.params.file}`);
+});
+
+// Listen.
 app.listen(3001, () => {
   console.log('Server listening on port 3001');
 });
